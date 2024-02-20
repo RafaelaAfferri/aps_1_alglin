4085c3d67a1275b14810d6f358699eedf170ae7f
RafaelaAfférri <rafaelaao@al.insper.edu.br>
2024-02-20 09:22:12 -0300

    jogo minimamente funcional

--- classes.py
+++ classes.py
@@ -18,7 +18,7 @@ class Shooter(pygame.sprite.Sprite):
 
 
 class Bullet(pygame.sprite.Sprite):
-    def __init__(self, s0, v0, celestial_body_1):
+    def __init__(self, s0, v0):
         pygame.sprite.Sprite.__init__(self)
 
 
@@ -29,19 +29,25 @@ class Bullet(pygame.sprite.Sprite):
         self.image = pygame.Surface((10, 10))
         self.image.fill((0, 0, 255))
         self.rect = pygame.Rect(self.s0[0], self.s0[1], 10, 10)
+        self.points = 0
         
 
 
-    def update(self, celestial_body_1):
+    def update(self, celestial_body_1, celestial_body_2):
 
         #Gravitational force
         distance = np.linalg.norm(celestial_body_1.s0 - self.s0)
         gravtional_direction = (celestial_body_1.s0 - self.s0) / distance
-        gravtional_force = (celestial_body_1.constant / distance**2) * gravtional_direction
+        gravtional_force_1 = (celestial_body_1.constant / distance**2) * gravtional_direction
+
+        distance = np.linalg.norm(celestial_body_2.s0 - self.s0)
+        gravtional_direction = (celestial_body_2.s0 - self.s0) / distance
+        gravtional_force_2 = (celestial_body_2.constant / distance**2) * gravtional_direction
+
 
 
         #Update velocity and position
-        self.v0 = self.v0 + gravtional_force
+        self.v0 = self.v0 + gravtional_force_1 + gravtional_force_2
         self.s0 += self.v0
         self.rect.center = self.s0
         
@@ -51,23 +57,38 @@ class Bullet(pygame.sprite.Sprite):
         screen.blit(self.image, self.rect)
         pygame.display.update()
 
-    def collision(self, celestial_body_1):
-        return self.rect.colliderect(celestial_body_1.rect)
+    def collision(self, body):
+        return self.rect.colliderect(body.rect)
         
 
 
-class Celestial_body_1(pygame.sprite.Sprite):
-    def __init__(self):
+class Celestial_body(pygame.sprite.Sprite):
+    def __init__(self, s0, constant, size):
 
         pygame.sprite.Sprite.__init__(self)
 
-        self.s0 = np.array([300, 100])
-        self.image = pygame.Surface((50, 50))
+        self.s0 = s0
+        self.image = pygame.Surface(size)
         self.image.fill((0, 225, 50))
-        self.rect = pygame.Rect(self.s0[0], self.s0[1], 50, 50)
-        self.constant = 1000
+        self.rect = pygame.Rect(self.s0[0], self.s0[1],size[0], size[1])
+        self.constant = constant
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
+
+class Target(pygame.sprite.Sprite):
+    def __init__(self, s0, size):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.s0 = s0
+        self.image = pygame.Surface(size)
+        self.image.fill((0, 100, 100))
+        self.rect = pygame.Rect(self.s0[0], self.s0[1], size[0], size[1])
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+
+
+
 
 
--- main.py
+++ main.py
@@ -12,7 +12,9 @@ FPS = 60
 
 #Incializate obejcts
 shooter = Shooter()
-celestial_body_1 = Celestial_body_1()
+celestial_body_1 = Celestial_body(np.array([300, 100]),1000, (40,40))
+celestial_body_2 = Celestial_body(np.array([100, 100]),800, (50,50))
+target = Target(np.array([200, 200]), (30, 30))
 
 #Game colors
 BLACK = (0, 0, 0)
@@ -20,6 +22,8 @@ BLACK = (0, 0, 0)
 #Game variables
 shoot = False
 bullet_list = []
+control_velocity = 100
+jogo = True
 
 
 
@@ -29,6 +33,7 @@ running = True
 while running:
 
     clock.tick(FPS)
+
 
     #EVENTS
     for event in pygame.event.get():
@@ -41,11 +46,11 @@ while running:
         if shoot:
             y = pygame.mouse.get_pos()
             v = y - shooter.s0
-            v = v/np.linalg.norm(v)
+            v = v/control_velocity
 
             
 
-            bullet = Bullet(shooter.s0, v*2, celestial_body_1)
+            bullet = Bullet(shooter.s0, v*2)
 
             bullet_list.append(bullet)
             
@@ -58,20 +63,23 @@ while running:
 
     #Update bullets according to the celestial body
     for bullet in bullet_list:
-        bullet.update(celestial_body_1)
+        bullet.update(celestial_body_1, celestial_body_2)
 
 
     #Delete bullets that go off screen
-    new_bullets = []
     for bullet in bullet_list:
-        if pygame.Rect(0, 0, 400, 400).colliderect(bullet.rect):
-            bullet.kill()
-            # new_bullets.append(bullet)
-        if bullet.collision(celestial_body_1):
-            bullet.kill()
-            # new_bullets.append(bullet)
-    # bullet_list = new_bullets
+        if not pygame.Rect(0, 0, 400, 400).colliderect(bullet.rect):
+            bullet_list.remove(bullet)
+        elif bullet.collision(celestial_body_1):
+            bullet_list.remove(bullet)
+        elif bullet.collision(celestial_body_2):
+            bullet_list.remove(bullet)
+        elif bullet.collision(target):
+            bullet_list.remove(bullet)
+            bullet.points += 1
+
             
+    
     
     #DRAW OBJECTS
 
@@ -83,6 +91,10 @@ while running:
 
     #Draw celestial body
     celestial_body_1.draw(screen)
+    celestial_body_2.draw(screen)
+
+    #Draw target
+    target.draw(screen)
 
     #Draw bullets
     for bullet in bullet_list:
